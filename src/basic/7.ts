eb046527b3630a90ddf4f824b2ceb867f8819fbd
InnaUsenko <innausenko8@gmail.com>
2024-10-15 15:36:38 +0300

    basic 4-7

---
+++ src/basic/7.ts
@@ -0,0 +1,28 @@
+type MyArticle = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1: MyArticle = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2: MyArticle = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
