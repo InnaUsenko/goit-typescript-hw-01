03dda2817536a2d45f9ec1c059b9eb1525782e3d
InnaUsenko <innausenko8@gmail.com>
2024-10-15 16:52:43 +0300

    generic 5-6

---
+++ src/generic/6.ts
@@ -0,0 +1,19 @@
+type Errors = {
+  email?: string[];
+  firstName?: string[];
+  lastName?: string[];
+  phone?: string[];
+};
+
+type Form = {
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  phone: string | null;
+  errors: Errors;
+};
+
+// Реалізуйте Params так,
+// щоб унеможливити поле 'errors' з типу Form
+// Omit<Type, Keys>
+type Params = Omit<Form, "errors">;
